174161ac3140c1cf08aa4653e1262f4525bff274
dioh0922 <diohound@gmail.com>
2022-11-13 11:59:09 +0900

    Node用の整理

--- src/front/front.js
+++ src/front/front.js
@@ -1,19 +1,17 @@
-import {createApp} from 'vue';
-const axios = require("axios");
-import App from "./component/app.vue";
 
+import {createApp} from 'vue';
+import App from "./component/app.vue";
 const app_main = createApp({
 	mounted(){
-		/*git rm --cached file-not-to-track.html*/
+		//git rm --cached file-not-to-track.html
 		alert("create");
 	}
 });
 
 app_main.mount("#container");
 
-/*
+
 (window.onload = () => {
 	//container
 	alert("load");
 });
-*/
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-//const webpack = require("webpack");
+const webpack = require("webpack");
 const path = require("path");
 const {VueLoaderPlugin} = require("vue-loader");
 module.exports = {
@@ -10,7 +10,34 @@ module.exports = {
 		filename:"[name].js"
 	},
 	resolve:{
-		extensions: ["", ".js", ".vue"]
+		extensions: ["", ".js", ".vue"],
+		fallback: {
+			//Nodejs系はwebpack5だと自分で持ってくる
+			"fs": false,
+			assert: require.resolve('assert'),
+       buffer: require.resolve('buffer'),
+       console: require.resolve('console-browserify'),
+       constants: require.resolve('constants-browserify'),
+       crypto: require.resolve('crypto-browserify'),
+       domain: require.resolve('domain-browser'),
+       events: require.resolve('events'),
+       http: require.resolve('stream-http'),
+       https: require.resolve('https-browserify'),
+       os: require.resolve('os-browserify/browser'),
+       path: require.resolve('path-browserify'),
+       punycode: require.resolve('punycode'),
+       process: require.resolve('process/browser'),
+       querystring: require.resolve('querystring-es3'),
+       stream: require.resolve('stream-browserify'),
+       string_decoder: require.resolve('string_decoder'),
+       sys: require.resolve('util'),
+       timers: require.resolve('timers-browserify'),
+       tty: require.resolve('tty-browserify'),
+       url: require.resolve('url'),
+       util: require.resolve('util'),
+       vm: require.resolve('vm-browserify'),
+       zlib: require.resolve('browserify-zlib'),
+		}
 	},
 	devtool: "inline-source-map",
 	module:{
@@ -34,6 +61,9 @@ module.exports = {
 		]
 	},
 	plugins:[
-		new VueLoaderPlugin()
-	]
+		new VueLoaderPlugin(),
+		new webpack.ProvidePlugin({
+			process: "process/browser",
+		}),
+	],
 }
