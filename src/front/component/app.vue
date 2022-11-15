c2fec61e8007566de3a93e0205d29eef676f6595
dioh0922 <diohound@gmail.com>
2022-11-15 10:46:02 +0900

    初期読み込み開始

--- src/front/component/app.vue
+++ src/front/component/app.vue
@@ -6,8 +6,14 @@
 
 <script>
  import Form from "./Form.vue";
+ import axios from "axios";
  export default {
 	 mounted(){
+		 axios.get("./Task").then(res => {
+			 console.log(res);
+		 }).catch(er => {
+
+		 });
 		 this.list = [
 			 {id:0, label:"test"},{id:1, label:"test1"}
 		 ];
