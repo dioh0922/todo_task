e285f8c385fac7e7ce597caa5f39021b94c5dc88
dioh0922 <diohound@gmail.com>
2022-12-08 11:00:40 +0900

    名称変更

--- src/front/component/Detail.vue
+++ src/front/component/Detail.vue
@@ -5,17 +5,27 @@
 		<p>タスク内容</p>
 		<p>開始：{{project.date}}</p>
 		<textarea placeholder="内容"></textarea>
-		<table>
+		<table >
 			<tr v-for="log in project.log">
 				<td>ブレスト1</td>
 				<td>{{log}}</td>
 			</tr>
+			<tr>
+				<td><textarea v-model="log_txt"></textarea></td>
+				<td><button v-on:click='$emit("add-log", this.log_txt)'>+</button></td>
+			</tr>
+		</table>
+		\\\\\\\\
+		<table >
 			<tr v-for="ref in project.ref">
 				<td>2</td>
 				<td>{{ref}}</td>
 			</tr>
+			<tr>
+				<td><textarea v-model="ref_txt"></textarea></td>
+				<td><button v-on:click='$emit("add-ref", this.ref_txt)'>+</button></td>
+			</tr>
 		</table>
-
 	</div>
 </template>
 
@@ -26,6 +36,8 @@
 	 },
  	data(){
  		return {
+			log_txt:"",
+			ref_txt:"",
  		};
  	}
  }
